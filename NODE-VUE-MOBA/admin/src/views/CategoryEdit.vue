d9b68a3de69fc1bd8da2472c230a4f205e5ea0c0
milk-abc <3292944779@qq.com>
2021-07-11 17:04:02 +0800

    nodejs学习

--- NODE-VUE-MOBA/admin/src/views/CategoryEdit.vue
+++ NODE-VUE-MOBA/admin/src/views/CategoryEdit.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>新建分类</h1>
+    <h1>{{id ? '编辑' : '新建'}}分类</h1>
     <el-form label-width="120px"
              @submit.native.prevent="save">
       <el-form-item label="名称">
@@ -16,6 +16,9 @@
 <script>
 
 export default {
+  props: {
+    id: {}
+  },
   data () {
     return {
       model: {
@@ -25,14 +28,25 @@ export default {
   },
   methods: {
     async save () {
-      console.log('save')
-      await this.$http.post('categories', this.model)
+      if (this.id) {
+        await this.$http.put(`categories/${this.id}`, this.model)
+      } else {
+        await this.$http.post('categories', this.model)
+      }
+
       this.$router.push('/categories/list')
       this.$message({
         type: 'success',
         message: '保存成功'
       })
+    },
+    async fetch () {
+      const res = await this.$http.get(`categories/${this.id}`)
+      this.model = res.data
     }
+  },
+  created () {
+    this.id && this.fetch()
   }
 }
 </script>
--- NODE-VUE-MOBA/admin/src/views/CategoryList.vue
+++ NODE-VUE-MOBA/admin/src/views/CategoryList.vue
@@ -9,6 +9,18 @@
       <el-table-column prop="name"
                        label="分类名称">
       </el-table-column>
+      <el-table-column fixed="right"
+                       label="操作"
+                       width="180">
+        <template slot-scope="scope">
+          <el-button type="text"
+                     size="small"
+                     @click="$router.push(`/categories/edit/${scope.row._id}`)">编辑</el-button>
+          <el-button type="text"
+                     size="small"
+                     @click="remove(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
@@ -24,6 +36,25 @@ export default {
     async fetch () {
       const res = await this.$http.get('categories')
       this.items = res.data
+    },
+    async remove (row) {
+      this.$confirm(`是否确定要删除分类${row.name}`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(async () => {
+        await this.$http.delete(`categories/${row._id}`)
+        this.$message({
+          type: 'success',
+          message: '删除成功!'
+        });
+        this.fetch()
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
     }
   },
   created () {

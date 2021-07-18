7eff27831e236bb4199a6ac58fcdbba5f2bfe1e1
milk-abc <3292944779@qq.com>
2021-07-18 19:38:53 +0800

    feat:王者荣耀管理系统简单版开发

--- NODE-VUE-MOBA/admin/src/router/index.js
+++ NODE-VUE-MOBA/admin/src/router/index.js
@@ -6,6 +6,10 @@ import CategoryEdit from "../views/CategoryEdit.vue";
 import CategoryList from "../views/CategoryList.vue";
 import ItemEdit from "../views/ItemEdit.vue";
 import ItemList from "../views/ItemList.vue";
+import HeroEdit from "../views/HeroEdit.vue";
+import HeroList from "../views/HeroList.vue";
+import ArticleEdit from "../views/ArticleEdit.vue";
+import ArticleList from "../views/ArticleList.vue";
 Vue.use(VueRouter);
 
 const routes = [
@@ -20,6 +24,12 @@ const routes = [
       { path: "/items/create", component: ItemEdit },
       { path: "/items/edit/:id", component: ItemEdit, props: true },
       { path: "/items/list", component: ItemList },
+      { path: "/heroes/create", component: HeroEdit },
+      { path: "/heroes/edit/:id", component: HeroEdit, props: true },
+      { path: "/heroes/list", component: HeroList },
+      { path: "/articles/create", component: ArticleEdit },
+      { path: "/articles/edit/:id", component: ArticleEdit, props: true },
+      { path: "/articles/list", component: ArticleList },
     ],
   },
 ];
